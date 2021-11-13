7d0073d67d23f653c051cd5db29fdd33493f7458
pythagore1123 <ledinhhai1312@gmail.com>
2021-11-13 14:13:22 +0700

    Updates

---
+++ precache-manifest.4c4c3094e6a587393ad1bbf0229da5b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a27cadc5221be9718da161f36f2d27f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfd224de66e9fa14ef97",
+    "url": "/static/css/main.e9536d50.chunk.css"
+  },
+  {
+    "revision": "8b7681ab0ea77e0d43a9",
+    "url": "/static/js/2.3bdcbeb5.chunk.js"
+  },
+  {
+    "revision": "8f06b9e0c45e6669bf287c6947cdbc57",
+    "url": "/static/js/2.3bdcbeb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2733c8d9702fdf9e8a5d",
+    "url": "/static/js/3.83e60133.chunk.js"
+  },
+  {
+    "revision": "dfd224de66e9fa14ef97",
+    "url": "/static/js/main.822f7d88.chunk.js"
+  },
+  {
+    "revision": "2986f5265558f49a6169",
+    "url": "/static/js/runtime-main.be2e7c64.js"
+  }
+]);
